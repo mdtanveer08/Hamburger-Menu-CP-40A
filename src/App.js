65ccaf43d81779fe38f6331072d8d51da5b0eb89
mdtanveero8 <md.t.r.rehman@gmail.com>
2024-11-07 09:58:48 +0000

    commit messge

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {Route, Switch} from 'react-router-dom'
+import NotFound from './components/NotFound'
+import Home from './components/Home'
+import About from './components/About'
+
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
+)
+
+export default App
